582ddcc882f0c7b112cb5a8cf54003d107a940e1
Thomas Haferlach <t.haferlach@gmail.com>
2021-06-08 17:32:03 +0200

    Revert reversions

--- scripts/generate_song_data_beam.py
+++ scripts/generate_song_data_beam.py
@@ -25,8 +25,8 @@ from apache_beam.metrics import Metrics
 from magenta.models.music_vae import TrainedModel
 import note_seq
 
-from .. import config
-from ../utils/ import song_utils
+import config
+from utils import song_utils
 
 FLAGS = flags.FLAGS
 
@@ -103,7 +103,7 @@ def main(argv):
       FLAGS.pipeline_options.split(','))
 
   with beam.Pipeline(options=pipeline_options) as p:
-    p |= 'tfrecord_list' >> beam.Create(FLAGS.input)
+    p |= 'tfrecord_list' >> beam.Create([FLAGS.input])
     p |= 'read_tfrecord' >> beam.io.tfrecordio.ReadAllFromTFRecord(
         coder=beam.coders.ProtoCoder(note_seq.NoteSequence))
     p |= 'shuffle_input' >> beam.Reshuffle()
